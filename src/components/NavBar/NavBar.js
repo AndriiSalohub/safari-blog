fe0710d772d396dbdb1b17160e773131ecac4fd9
AndriiSalohub <aominedaikivs@gmail.com>
2022-06-05 12:31:59 +0300

    delete buy-now page and change it to move on another page

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,199 +7,222 @@ import "./NavBar.css";
 export const NavBar = () => {
     const { scrollY } = useViewportScroll(0);
     const offsetY = [0, 150];
-  const heightSizesSm = [110, 70];
-  const heightSizesLg = [150, 70];
-  const marginSizes = [30, 10];
-  const heightSm = useTransform(scrollY, offsetY, heightSizesSm);
-  const heightLg = useTransform(scrollY, offsetY, heightSizesLg);
-  const margin = useTransform(scrollY, offsetY, marginSizes);
-  const [isOpen, setOpen] = useState(false);
+    const heightSizesSm = [110, 70];
+    const heightSizesLg = [150, 70];
+    const marginSizes = [30, 10];
+    const heightSm = useTransform(scrollY, offsetY, heightSizesSm);
+    const heightLg = useTransform(scrollY, offsetY, heightSizesLg);
+    const margin = useTransform(scrollY, offsetY, marginSizes);
+    const [isOpen, setOpen] = useState(false);
 
-  if (window.screen.width < 1400) {
-    return (
-      <>
-        <motion.div
-          className="nav-bar"
-          style={{
-            heightSm,
-          }}
-        >
-          <div className="nav-bar-logo">
-            <motion.img
-              src="https://themes.muffingroup.com/be/safari2/wp-content/uploads/2019/04/logo-retina.png"
-              alt=""
-              style={{
-                  marginTop: margin,
-                  marginBottom: margin,
-              }}
-            />
-          </div>
-          <div className="nav-bar-humburger-menu-icon">
-            {" "}
-            <Hamburger
-              toggled={isOpen}
-              toggle={setOpen}
-              color="#b0893d"
-              distance="lg"
-              size={22}
-            />
-          </div>
-          <ul className={isOpen ? "nav-bar-menu active-menu" : "nav-bar-menu"}>
-            <li className="nav-bar-menu-item">
-              <NavLink
-                to="/"
-                className={(navData) =>
-                  navData.isActive ? "nav-link-selected-hamburger" : ""
-                }
-              >
-                {" "}
-                HOME
-              </NavLink>
-            </li>
-            <li className="nav-bar-menu-item ">
-              <NavLink
-                to="/our-travels"
-                className={(navData) =>
-                  navData.isActive ? "nav-link-selected-hamburger" : ""
-                }
-              >
-                {" "}
-                OUR TRAVELS
-              </NavLink>
-            </li>
-            <li className="nav-bar-menu-item ">
-              <NavLink
-                to="/offer"
-                className={(navData) =>
-                  navData.isActive ? "nav-link-selected-hamburger" : ""
-                }
-              >
-                {" "}
-                OFFER
-              </NavLink>
-            </li>
-            <li className="nav-bar-menu-item ">
-              <NavLink
-                to="/about"
-                className={(navData) =>
-                  navData.isActive ? "nav-link-selected-hamburger" : ""
-                }
-              >
-                {" "}
-                ABOUT
-              </NavLink>
-            </li>
+    if (window.screen.width < 1400) {
+        return (
+            <>
+                <motion.div
+                    className="nav-bar"
+                    style={{
+                        heightSm,
+                    }}
+                >
+                    <div className="nav-bar-logo">
+                        <motion.img
+                            src="https://themes.muffingroup.com/be/safari2/wp-content/uploads/2019/04/logo-retina.png"
+                            alt=""
+                            style={{
+                                marginTop: margin,
+                                marginBottom: margin,
+                            }}
+                        />
+                    </div>
+                    <div className="nav-bar-humburger-menu-icon">
+                        {" "}
+                        <Hamburger
+                            toggled={isOpen}
+                            toggle={setOpen}
+                            color="#b0893d"
+                            distance="lg"
+                            size={22}
+                        />
+                    </div>
+                    <ul
+                        className={
+                            isOpen ? "nav-bar-menu active-menu" : "nav-bar-menu"
+                        }
+                    >
+                        <li className="nav-bar-menu-item">
+                            <NavLink
+                                to="/"
+                                className={(navData) =>
+                                    navData.isActive
+                                        ? "nav-link-selected-hamburger"
+                                        : ""
+                                }
+                            >
+                                {" "}
+                                HOME
+                            </NavLink>
+                        </li>
+                        <li className="nav-bar-menu-item ">
+                            <NavLink
+                                to="/our-travels"
+                                className={(navData) =>
+                                    navData.isActive
+                                        ? "nav-link-selected-hamburger"
+                                        : ""
+                                }
+                            >
+                                {" "}
+                                OUR TRAVELS
+                            </NavLink>
+                        </li>
+                        <li className="nav-bar-menu-item ">
+                            <NavLink
+                                to="/offer"
+                                className={(navData) =>
+                                    navData.isActive
+                                        ? "nav-link-selected-hamburger"
+                                        : ""
+                                }
+                            >
+                                {" "}
+                                OFFER
+                            </NavLink>
+                        </li>
+                        <li className="nav-bar-menu-item ">
+                            <NavLink
+                                to="/about"
+                                className={(navData) =>
+                                    navData.isActive
+                                        ? "nav-link-selected-hamburger"
+                                        : ""
+                                }
+                            >
+                                {" "}
+                                ABOUT
+                            </NavLink>
+                        </li>
 
-            <li className="nav-bar-menu-item ">
-              <NavLink
-                to="/contact"
-                className={(navData) =>
-                  navData.isActive ? "nav-link-selected-hamburger" : ""
-                }
-              >
-                {" "}
-                CONTACT
-              </NavLink>
-            </li>
-            <li className="nav-bar-menu-item ">
-              <NavLink
-                to="/buy-now"
-                className={(navData) =>
-                  navData.isActive ? "nav-link-selected-hamburger" : ""
-                }
-              >
-                BUY NOW
-              </NavLink>
-            </li>
-          </ul>
-        </motion.div>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <motion.div className="navigation-bar" style={{
-            height: heightLg,
-        }} >
-          <ul className="navigation-bar-menu">
-            <li className="navigation-bar-menu-item">
-              <NavLink
-                to="/"
-                className={(navData) =>
-                  navData.isActive ? "nav-link-selected" : ""
-                }
-              >
-                {" "}
-                HOME
-              </NavLink>
-            </li>
-            <li className="navigation-bar-menu-item ">
-              <NavLink
-                to="/our-travels"
-                className={(navData) =>
-                  navData.isActive ? "nav-link-selected" : ""
-                }
-              >
-                {" "}
-                OUR TRAVELS
-              </NavLink>
-            </li>
-            <li className="navigation-bar-menu-item ">
-              <NavLink
-                to="/offer"
-                className={(navData) =>
-                  navData.isActive ? "nav-link-selected" : ""
-                }
-              >
-                {" "}
-                OFFER
-              </NavLink>
-            </li>
-            <li className="navigation-bar-menu-img">
-              <Link to="/">
-                {" "}
-                <img
-                  src="https://themes.muffingroup.com/be/safari2/wp-content/uploads/2019/04/logo-retina.png"
-                  alt=""
-                  className="navigation-bar-menu-img-logo"
-                />
-              </Link>
-            </li>
-            <li className="navigation-bar-menu-item ">
-              <NavLink
-                to="/about"
-                className={(navData) =>
-                  navData.isActive ? "nav-link-selected" : ""
-                }
-              >
-                {" "}
-                ABOUT
-              </NavLink>
-            </li>
-            <li className="navigation-bar-menu-item ">
-              <NavLink
-                to="/contact"
-                className={(navData) =>
-                  navData.isActive ? "nav-link-selected" : ""
-                }
-              >
-                {" "}
-                CONTACT
-              </NavLink>
-            </li>
-            <li className="navigation-bar-menu-item ">
-              <NavLink
-                to="/buy-now"
-                className={(navData) =>
-                  navData.isActive ? "nav-link-selected" : ""
-                }
-              >
-                BUY NOW
-              </NavLink>
-            </li>
-          </ul>
-        </motion.div>
-      </>
-    );
-  }
+                        <li className="nav-bar-menu-item ">
+                            <NavLink
+                                to="/contact"
+                                className={(navData) =>
+                                    navData.isActive
+                                        ? "nav-link-selected-hamburger"
+                                        : ""
+                                }
+                            >
+                                {" "}
+                                CONTACT
+                            </NavLink>
+                        </li>
+                        <li className="nav-bar-menu-item ">
+                            <a
+                                href="https://themeforest.net/item/betheme-responsive-multipurpose-wordpress-theme/7758048?ref=muffingroup&irgwc=1&clickid=VKtTUmX0MxyIWlszN%3A0wnXCVUkD2Wx3T22cJU00&iradid=275988&irpid=1289117&iradtype=ONLINE_TRACKING_LINK&irmptype=mediapartner&mp_value1=&utm_campaign=af_impact_radius_1289117&utm_medium=affiliate&utm_source=impact_radius"
+                                target="_blank"
+                                rel="noreferrer"
+                                className={(navData) =>
+                                    navData.isActive
+                                        ? "nav-link-selected-hamburger"
+                                        : ""
+                                }
+                            >
+                                BUY NOW
+                            </a>
+                        </li>
+                    </ul>
+                </motion.div>
+            </>
+        );
+    } else {
+        return (
+            <>
+                <motion.div
+                    className="navigation-bar"
+                    style={{
+                        height: heightLg,
+                    }}
+                >
+                    <ul className="navigation-bar-menu">
+                        <li className="navigation-bar-menu-item">
+                            <NavLink
+                                to="/"
+                                className={(navData) =>
+                                    navData.isActive ? "nav-link-selected" : ""
+                                }
+                            >
+                                {" "}
+                                HOME
+                            </NavLink>
+                        </li>
+                        <li className="navigation-bar-menu-item ">
+                            <NavLink
+                                to="/our-travels"
+                                className={(navData) =>
+                                    navData.isActive ? "nav-link-selected" : ""
+                                }
+                            >
+                                {" "}
+                                OUR TRAVELS
+                            </NavLink>
+                        </li>
+                        <li className="navigation-bar-menu-item ">
+                            <NavLink
+                                to="/offer"
+                                className={(navData) =>
+                                    navData.isActive ? "nav-link-selected" : ""
+                                }
+                            >
+                                {" "}
+                                OFFER
+                            </NavLink>
+                        </li>
+                        <li className="navigation-bar-menu-img">
+                            <Link to="/">
+                                {" "}
+                                <img
+                                    src="https://themes.muffingroup.com/be/safari2/wp-content/uploads/2019/04/logo-retina.png"
+                                    alt=""
+                                    className="navigation-bar-menu-img-logo"
+                                />
+                            </Link>
+                        </li>
+                        <li className="navigation-bar-menu-item ">
+                            <NavLink
+                                to="/about"
+                                className={(navData) =>
+                                    navData.isActive ? "nav-link-selected" : ""
+                                }
+                            >
+                                {" "}
+                                ABOUT
+                            </NavLink>
+                        </li>
+                        <li className="navigation-bar-menu-item ">
+                            <NavLink
+                                to="/contact"
+                                className={(navData) =>
+                                    navData.isActive ? "nav-link-selected" : ""
+                                }
+                            >
+                                {" "}
+                                CONTACT
+                            </NavLink>
+                        </li>
+                        <li className="navigation-bar-menu-item ">
+                            <a
+                                href="https://themeforest.net/item/betheme-responsive-multipurpose-wordpress-theme/7758048?ref=muffingroup&irgwc=1&clickid=VKtTUmX0MxyIWlszN%3A0wnXCVUkD2Wx3T22cJU00&iradid=275988&irpid=1289117&iradtype=ONLINE_TRACKING_LINK&irmptype=mediapartner&mp_value1=&utm_campaign=af_impact_radius_1289117&utm_medium=affiliate&utm_source=impact_radius"
+                                target="_blank"
+                                rel="noreferrer"
+                                className={(navData) =>
+                                    navData.isActive ? "nav-link-selected" : ""
+                                }
+                            >
+                                BUY NOW
+                            </a>
+                        </li>
+                    </ul>
+                </motion.div>
+            </>
+        );
+    }
 };
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,25 +1,29 @@
 import React from "react";
+
 import { Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 import { NavBar } from "../../components/NavBar/NavBar";
-import { AboutPage } from "../../pages/AboutPage/AboutPage";
-import { HomePage } from "../../pages/HomePage/HomePage";
-import { OfferPage } from "../../pages/OfferPage/OfferPage";
 import { QuoteSlider } from "../../components/QuoteSlider/QuoteSlider";
 import { Introduce } from "../../components/Introduce/Introduce";
 import { SliderList } from "../../components/SliderList/SliderList";
 import { TripInfo } from "../../components/TripInfo/TripInfo";
 import { VideoPopup } from "../../components/VideoPopup/VideoPopup";
-import { UpperFooter } from "../../components/UpperFooter/UpperFooter";
-import "./App.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import { Footer } from "../../components/Footer/Footer";
 import { ArticlesList } from "../../components/Articles/ArticlesList";
 import { OurMission } from "../../components/OurMission/OurMission";
-import { ArticlesPage } from "../../pages/ArticlesPage/ArticlesPage";
-import { useSelector } from "react-redux";
 import { Authours } from "../../components/Authours/Authours";
 import { OurAuthoursInformation } from "../../components/OurAuthoursInformation/OurAuthoursInformation";
+import { UpperFooter } from "../../components/UpperFooter/UpperFooter";
+import { Footer } from "../../components/Footer/Footer";
+
+import { AboutPage } from "../../pages/AboutPage/AboutPage";
+import { HomePage } from "../../pages/HomePage/HomePage";
+import { OfferPage } from "../../pages/OfferPage/OfferPage";
+import { ArticlesPage } from "../../pages/ArticlesPage/ArticlesPage";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./App.css";
 
 export const App = () => {
     const articlePath = useSelector((state) => state.pathDetect);
@@ -71,15 +75,6 @@ export const App = () => {
                 />
                 <Route
                     path="/contact"
-                    element={
-                        <div>
-                            {" "}
-                            <NavBar /> <UpperFooter /> <Footer />
-                        </div>
-                    }
-                />
-                <Route
-                    path="/buy-now"
                     element={
                         <div>
                             {" "}
