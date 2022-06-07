2d4f44783921f4ad442a28b4f0afd33297c2147d
AndriiSalohub <aominedaikivs@gmail.com>
2022-06-07 11:42:03 +0300

    add full version of validation

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import "./ContactForm.scss";
 
 export const ContactForm = () => {
